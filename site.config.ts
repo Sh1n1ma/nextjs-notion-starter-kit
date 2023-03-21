65bfe5c794b73986c2ab8967dfe85f2d6fbf3274
Sh1n1ma <68672629+Sh1n1ma@users.noreply.github.com>
2023-03-21 14:36:22 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: '阿丑',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'website for ashuai&xiaomao',
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
